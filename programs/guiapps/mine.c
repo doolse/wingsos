fe6430909965efba412bd3cf75c6f4cfb765eb9f
gregnacu <gregnacu>
2002-07-08 03:36:11 +0000

    Finally added an Exit button at the top right corner, opposite the replay button.  And made the window size itself properly on startting mine.

--- programs/guiapps/mine.c
+++ programs/guiapps/mine.c
@@ -18,8 +18,12 @@ void Replay() {
 	JWinReDraw(Board);
 }
 
+void ExitMine() {
+  exit(1);
+}
+
 int main() {
-	void *Appl,*Window,*But;
+	void *Appl,*Window,*But, *ExitBut;
 	
 	bmem = xmalloc(65536);
 	Appl = JAppInit(NULL,0);
@@ -27,12 +31,16 @@ int main() {
 	JAppSetMain(Appl, Window);
 	Board = JWinInit(NULL, 0, 24, 8,8, Window, WEV_MotionBut|WEV_Button, 0);
 	But = JButInit(NULL, Window, 0, "Replay");
+        ExitBut = JButInit(NULL, Window, 0, "Exit");
 	JWinMove(But, 0,0, 0);
+        JWinMove(ExitBut, 104,0, 0);
 	JWinOveride(But, MJBut_Clicked, Replay);
+        JWinOveride(ExitBut, MJBut_Clicked, ExitMine);
 	JWinOveride(Board, MJW_Draw, DrawBoard);
 	JWinOveride(Board, MJW_Button, DoButton);
 	JWinOveride(Board, MJW_RButton, DoRButton);
 	JWinSize(Board, boardx*8+8, boardy*8+8);
+        JWinSize(Window, boardx*8, boardy*8+24);
 	PrepBoard();
 	JWinShow(Window);
 	JAppLoop(Appl);
