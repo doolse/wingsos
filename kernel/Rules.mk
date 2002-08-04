46494ee806e96d31b7c4286769ccf0c8175ceb7a
jmaginni <jmaginni>
2002-08-04 09:03:03 +0000

    *** empty log message ***

--- kernel/Rules.mk
+++ kernel/Rules.mk
@@ -14,7 +14,8 @@ $Okernel.o65: $(KERNOBJ) $(JL65)
 	$(JL65) -G -R -p -o $@ $(KERNOBJ)
 
 $Ojoskern.prg: $Okernel.o65 $(KERDIR)loader.a65 $Edebug/prserver.prg $(INITRD) $(AR65) $(DATA65)
-	$(AR65) -p $(INITRD) | pucrunch -d -c0 | $(DATA65) -o $Oinitrd.o65 -st -a 0x20000
+	$(AR65) -p $(INITRD) -o file.blah
+	pucrunch -d -c0 file.blah | $(DATA65) -o $Oinitrd.o65 -st -a 0x20000
 	$(DATA65) -o $Oprserver.o65 -st -c $Edebug/prserver.prg
 	$(JA) -e -o $@ $(KERDIR)loader.a65
 
