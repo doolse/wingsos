abd21198ccceff7435ba093c1192a13ebfe8c25f
jmaginni <jmaginni>
2003-01-05 08:01:41 +0000

    Makefile trouble

--- programs/guiapps/Rules.mk
+++ programs/guiapps/Rules.mk
@@ -6,4 +6,4 @@ $(GUIPRG): CFLAGS += -lwinlib -lfontlib
 $(BG)launch:  CFLAGS += -lunilib -lwinlib
 $(BPU)mine: $Oamine.o65
 $(BPG)jpeg: $Oajpeg.o65
-$(BG)winapp: CFLAGS += -Wl-t0x400
+$(BG)winapp: CFLAGS = -w -lwinlib -lfontlib -Wl-t0x400
