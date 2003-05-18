6f9db1a686fd13cda12918960d87a8672e77c641
gregnacu <gregnacu>
2003-05-18 10:43:20 +0000

    Added wordservices to the makefile.

--- programs/guiapps/Rules.mk
+++ programs/guiapps/Rules.mk
@@ -1,5 +1,5 @@
 VPATH += :$(PRGDIR)guiapps
-GUIPRG := $(BG)gui $(BG)backimg.hbm $(BG)winman $(BG)winapp $(BPU)mine $(BPG)jpeg $(BPU)j.jpg $(BG)search $(BG)launch $(BG)guitext 
+GUIPRG := $(BG)gui $(BG)backimg.hbm $(BG)winman $(BG)winapp $(BPU)mine $(BPG)jpeg $(BPU)j.jpg $(BG)search $(BG)launch $(BG)guitext $(BG)wordserve
 #$(BG)credits $(BPD)tutapp  
 ALLOBJ += $(GUIPRG)
 
