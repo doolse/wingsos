4d076b68df9d44e27d1624bd62717279726ca60e
jmaginni <jmaginni>
2004-02-18 11:35:53 +0000

    New includes

--- include/c/limits.h
+++ include/c/limits.h
@@ -1,12 +1,19 @@
 #ifndef _limits_h
 #define _limits_h
 
-#define INT_MAX 0x7fff
-#define SHRT_MAX 0x7fff
-#define USHRT_MAX 0xffffu
-#define LONG_MAX 0x7fffffff
-#define ULONG_MAX 0xffffffffu
-#define UINT_MAX 0xffffu
-#define LONG_MIN -0x80000000L
+#define INT_MAX (0x7fff)
+#define SHRT_MAX (0x7fff)
+#define USHRT_MAX (0xffffu)
+#define LONG_MAX (0x7fffffff)
+#define ULONG_MAX (0xffffffffUL)
+#define UINT_MAX (0xffffU)
+#define LONG_MIN (-0x80000000L)
+#define CHAR_MAX (0x7f)
+#define SCHAR_MAX (0x7f)
+#define UCHAR_MAX (0xff)
+#define CHAR_MIN (-0x80)
+#define SCHAR_MIN (-0x80)
+#define SHRT_MIN (-0x8000)
+#define INT_MIN (-0x8000)
 
 #endif
