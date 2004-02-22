b35d3b1dfd86da7cbfe84f1603f5a9eb2bc83cad
jmaginni <jmaginni>
2004-02-22 07:30:59 +0000

    *** empty log message ***

--- include/c/console.h
+++ include/c/console.h
@@ -28,7 +28,9 @@
 #define LC_Start	1
 #define LC_Full		2
 
+// FIXME ESC should be 27
 enum {
+ESC=96,
 TAB=9,
 F1=0x80,
 F2,F3,F4,F5,F6,F7,F8,
@@ -58,6 +60,59 @@ extern void con_modeonoff(int on, int off);
 
 extern int con_xsize;
 extern int con_ysize;
+
+//Custom GetLines ... 
+
+//doesn't scroll left and right, full editor for a max of size.
+
+extern char * getmyline(char * original,int size, int x, int y, int password);
+
+
+//displays a max line width of displaysize, with a larger max string size.
+//if the string is larger than the display, it scrolls left and right. 
+//can be given a restriction string. characters in the string can't be 
+//typed into the editor. 
+
+extern char * getmylinerestrict(char * original, long size, int displaysize, int x, int y, char * restrict, int password);
+
+
+//basic, not a full editor, used for fetching simple number inputs.
+
+extern char * getmylinen(int size, int x, int y);
+
+//message box object
+  
+typedef struct msgboxobj_s {
+  int numoflines; 
+  int showprogress;  
+  
+  char * msgline[3];  
+  
+  int progresswidth;  
+  ulong numofitems;
+  ulong progressposition;
+  int linelength;
+  
+  int top;
+  int bottom;
+  int left;
+  int right;
+    
+} msgboxobj;
+     
+msgboxobj * initmsgboxobj(char * msgline1, char * msgline2, char * 
+sgline3, int showprogress, ulong numofitems);
+void drawmsgboxobj(msgboxobj * mb);
+
+//Message box progress bar management
+
+void updatemsgboxprogress(msgboxobj * mb);
+void incrementprogress(msgboxobj * mb);
+void setprogress(msgboxobj * mb,ulong progress);
+
+//Simple message box call
+
+void drawmessagebox(char * string1, char * string2, int wait);
 
 
 #endif /* _CONSOLE_H */
